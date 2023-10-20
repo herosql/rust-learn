f58641ab9d90c2ea4da7ff82af7c8e001f1d64ff
herosql <15074798673@163.com>
2023-10-20 14:54:31 +0800

    feat:单线程web完成

--- src/example/web.rs
+++ src/example/web.rs
@@ -21,19 +21,13 @@ fn handle_connection(mut stream: TcpStream) {
     //     .collect();
 
     // println!("Request: {:#?}", http_request);
-    // let response = "HTTP/1.1 200 OK\r\n\r\n";
-    if request_line == "GET / HTTP/1.1" {
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("src/resource/hello.html").unwrap();
-        let length = contents.len();
-        let response = format!("{status_line}\r\nContent-Length:{length}\r\n\r\n{contents}");
-        stream.write_all(response.as_bytes()).unwrap();
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "src/resource/hello.html")
     } else {
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("src/resource/404.html").unwrap();
-        let length = contents.len();
-
-        let response = format!("{status_line}\r\nContent-Length:{length}\r\n\r\n{contents}");
-        stream.write_all(response.as_bytes()).unwrap();
-    }
+        ("HTTP/1.1 404 NOT FOUND", "src/resource/404.html")
+    };
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+    let response = format!("{status_line}\r\nContent-Length:{length}\r\n\r\n{contents}");
+    stream.write_all(response.as_bytes()).unwrap();
 }
